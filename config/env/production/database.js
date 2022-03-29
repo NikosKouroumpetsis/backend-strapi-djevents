d91dc6c4d72f0c1fd952185daf6c2a9e89382213
NikosKouroumpetsis <kourou67s@gmail.com>
2022-03-29 09:36:20 +0300

    try fix back resseting try one

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -2,7 +2,38 @@ const parse = require("pg-connection-string").parse;
 const config = parse(process.env.DATABASE_URL);
 
 module.exports = ({ env }) => ({
+  defaultConnection: "default",
+  connections: {
+    default: {
+      connector: "bookshelf",
+      settings: {
+        client: "postgres",
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        username: config.user,
+        password: config.password,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      },
+      options: {
+        ssl: true,
+      },
+    },
+  },
+});
+
+/*
+
+const parse = require("pg-connection-string").parse;
+const config = parse(process.env.DATABASE_URL);
+
+
+module.exports = ({ env }) => ({
+  defaultConnection: "default",
   connection: {
+    connector: "bookshelf",
     client: "postgres",
     connection: {
       host: config.host,
@@ -17,3 +48,6 @@ module.exports = ({ env }) => ({
     debug: false,
   },
 });
+
+
+*/
